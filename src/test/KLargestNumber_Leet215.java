929a10a7d5b8799b8c41849ec73f9eaff15b2d92
spe <1312201788@qq.com>
2018-10-19 15:33:21 +0800

    Leet215_第k大元素_mid

--- src/test/KLargestNumber_Leet215.java
+++ src/test/KLargestNumber_Leet215.java
@@ -6,14 +6,17 @@ public class KLargestNumber_Leet215 {
         // TODO Auto-generated method stub
 
         int[] nums = new int[]{3,2,3,1,2,4,5,5,6};
-        quickSort(nums, 0, nums.length-1);
+        //quickSort(nums, 0, nums.length-1);
 
-        System.out.println(findKthLargest(nums, 4));
-        System.out.println(findKthLargest2(nums, 4));
+        //System.out.println(findKthLargest(nums, 4));
+        //System.out.println(findKthLargest2(nums, 4));
+        System.out.println(findKthLargest3(nums, 4));
     }
     //寻找第k大的元素，我的想法是先排序，然后直接找，这样时间复杂度取决于排序算法时间复杂度，我写的是快拍，并不是很快
-    //网上思路：，遍历两遍，第一遍找到最大值和最小值，然后直接定义个这么大数组，那么所有取值减去最小值的差作为下标，放进来数量，然后就能找到第k个最大元素了
+    //网上思路1：，遍历两遍，第一遍找到最大值和最小值，然后直接定义个这么大数组，那么所有取值减去最小值的差作为下标，放进来数量，然后就能找到第k个最大元素了
 
+
+    //直接快排思路，142ms运行时间
     static public int findKthLargest(int[] nums, int k) {
         quickSort(nums, 0, nums.length-1);
 
@@ -48,7 +51,7 @@ public class KLargestNumber_Leet215 {
     }
 
 
-    //discuss思路
+    //discuss思路，这种思路时间复杂度其实还好，但是空间复杂度太大，运行时间6ms
     static public int findKthLargest2(int[] nums, int k){
         int max = nums[0];
         int min = nums[0];
@@ -74,5 +77,50 @@ public class KLargestNumber_Leet215 {
         return res;
     }
 
+
+
+    //大神思路，就是快排，但是每次快排特性在于能把一个点放到最终位置，那么判断这个位置和k的比较
+    //大于k就继续快排起左边，小于就快排其右边,时间57ms，但是优势在于不需要额外空间
+    //假如到了那种在100w个元素中查找时，就会很有利
+    static public int findKthLargest3(int[] nums, int k){
+        int index = 0;
+        int begin = 0;
+        int end = nums.length - 1;
+        while(true){
+            int temp = nums[begin];
+            int m = begin;
+            int n = end;
+            while(m < n){
+                while(n > m && nums[n] <= temp){
+                    n--;
+                }
+                if(n > m){
+                    nums[m] ^= nums[n];
+                    nums[n] ^= nums[m];
+                    nums[m] ^= nums[n];
+                }
+                while(m < n && nums[m] > temp){
+                    m++;
+                }
+                if(m < n){
+                    nums[m] ^= nums[n];
+                    nums[n] ^= nums[m];
+                    nums[m] ^= nums[n];
+                }
+            }
+            nums[m] = temp;
+            index = m;
+            if(index == k - 1){
+                break;
+            }else if(index > k - 1){
+                end = index - 1;
+            }else{
+                begin = index + 1;
+            }
+
+        }
+
+        return nums[index];
+    }
 
 }
